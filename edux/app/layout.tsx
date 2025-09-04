bb6f7f1656bf0e81d71ab54b6c0efea80b91c181
jim2107054 <mdjahid205hasan@gmail.com>
2025-09-04 19:21:05 +0600

    add all necessary components from shadcn

--- edux/app/layout.tsx
+++ edux/app/layout.tsx
@@ -27,7 +27,6 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <h1 className="text-4xl font-bold my-5">Welcome to Next.js!</h1>
         {children}
       </body>
     </html>
