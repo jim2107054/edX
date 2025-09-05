45fa4dd950e470828fcde08c0f4b51975181bf87
jim2107054 <mdjahid205hasan@gmail.com>
2025-09-05 10:54:37 +0600

    creating authentication

--- edux/lib/auth.ts
+++ edux/lib/auth.ts
@@ -1,8 +1,15 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
+import { prisma } from "./db";
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql", // or "mysql", "sqlite", ...etc
   }),
+  socialProviders:{
+    github:{
+      clientId: process.env.AUTH_GITHUB_CLIENT_ID,
+      clientSecret: process.env.AUTH_GITHUB_CLIENT_SECRET
+    }
+  }
 });
