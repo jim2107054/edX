e516bb0fe521ea7077a66aa56dc169ea43bd4b6b
jim2107054 <mdjahid205hasan@gmail.com>
2025-09-05 12:13:20 +0600

    instead of using evn variables directly, we use them via evn.ts

--- edux/lib/auth.ts
+++ edux/lib/auth.ts
@@ -1,6 +1,7 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./db";
+import { env } from "./env";
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
@@ -8,8 +9,8 @@ export const auth = betterAuth({
   }),
   socialProviders:{
     github:{
-      clientId: process.env.AUTH_GITHUB_CLIENT_ID,
-      clientSecret: process.env.AUTH_GITHUB_CLIENT_SECRET
+      clientId: env.AUTH_GITHUB_CLIENT_ID,
+      clientSecret: env.AUTH_GITHUB_CLIENT_SECRET
     }
   }
 });
