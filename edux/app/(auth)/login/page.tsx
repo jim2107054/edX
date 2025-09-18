4d057be27e5891adb191a068421f7b5463b10b2d
jim2107054 <mdjahid205hasan@gmail.com>
2025-09-18 19:52:02 +0600

    LoginPage

--- edux/app/(auth)/login/page.tsx
+++ edux/app/(auth)/login/page.tsx
@@ -1,92 +1,19 @@
-"use client";
+"use server"
 
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Label } from "@/components/ui/label";
-import { SelectSeparator } from "@/components/ui/select";
-import { Github, icons, Loader } from "lucide-react";
-import React, { useTransition } from "react";
-import { Input } from "@/components/ui/input";
-import { authClient } from "@/lib/auth-client";
-import { toast } from "sonner";
+import { auth } from "@/lib/auth";
+import { LoginForm } from "./_components/LoginForm";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-const LoginPage = () => {
-  const [githubPending, startGithubTransition] = useTransition();
+export default async function LoginPage() {
 
-  async function signInWithGithub() {
-    startGithubTransition(async () => {
-      try {
-        await authClient.signIn.social({
-          provider: "github",
-          callbackURL: "/",
-          fetchOptions: {
-            onSuccess: () => {
-              console.log("success");
-              toast.success("Logged in successfully");
-            },
-            onError: (e) => {
-              console.log("error", e);
-              toast.error(e.error.message);
-            },
-          },
-        });
-      } catch (e) {
-        toast.error(e instanceof Error ? e.message : "Something went wrong");
-      }
-    });
+  const session = await auth.api.getSession(
+    { headers: await headers() }
+  )
+
+  if (session) {
+    return redirect("/");
   }
 
-  return (
-    <Card className="px-4">
-      <CardHeader>
-        <CardTitle className="text-xl">Welcome back!</CardTitle>
-        <CardDescription>
-          Login with your github or email account
-        </CardDescription>
-      </CardHeader>
-      <CardContent className="flex flex-col gap-2">
-        <Button
-          disabled={githubPending}
-          onClick={signInWithGithub}
-          variant={"outline"}
-          className="w-full cursor-pointer"
-        >
-          {githubPending ? (
-            <>
-            <Loader className="size-5 animate-spin" />
-            <span>Loading....</span>
-            </>
-          ) : (
-            <>
-              <Github className="size-5" />
-              Login with Github
-            </>
-          )}
-        </Button>
-        <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
-          <span className="relative z-10 bg-card px-2 text-muted-foreground">
-            or continue with
-          </span>
-        </div>
-        <div className="grid gap-3">
-          <div className="grid gap-2">
-            <Label htmlFor="email" className="mb-1 mt-4">
-              Email
-            </Label>
-            <Input id="email" type="email" placeholder="jim@example.com" />
-          </div>
-          <Button className="w-full">Continue with Email</Button>
-        </div>
-        {/* <SelectSeparator className="my-4" /> */}
-      </CardContent>
-    </Card>
-  );
+  return <LoginForm />;
 };
-
-export default LoginPage;
