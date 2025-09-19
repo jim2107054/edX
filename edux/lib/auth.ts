58dcd5794f660ca2127e606faf37b35d32d6b2b1
jim2107054 <mdjahid205hasan@gmail.com>
2025-09-19 12:44:00 +0600

    add plugin to send OTP using sendVerificationOTP method  by betterAuth

--- edux/lib/auth.ts
+++ edux/lib/auth.ts
@@ -1,16 +1,31 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
+import { emailOTP } from "better-auth/plugins";
 import { prisma } from "./db";
 import { env } from "./env";
+import { resend } from "./resend";
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql", // or "mysql", "sqlite", ...etc
   }),
-  socialProviders:{
-    github:{
+  socialProviders: {
+    github: {
       clientId: env.AUTH_GITHUB_CLIENT_ID,
-      clientSecret: env.AUTH_GITHUB_CLIENT_SECRET
-    }
-  }
+      clientSecret: env.AUTH_GITHUB_CLIENT_SECRET,
+    },
+  },
+  plugins: [
+    emailOTP({
+      async sendVerificationOTP({ email, otp }) {
+        // Send the OTP to the user's email address
+          await resend.emails.send({
+          from: "EduX <onboarding@resend.dev>",
+          to: [email],
+          subject: "EduX - Verify your email",
+          html: `<p>Your verification code is: <strong>${otp}</strong></p>`,
+        });
+      },
+    }),
+  ],
 });
